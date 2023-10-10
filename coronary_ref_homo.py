890a2040fae2e12c67bfc44619e37b04f6266511
Alex-Karmazin <karmazinalex@gmail.com>
2023-10-11 00:55:08 +0300

    fix for bug with different genotype for same allele in different moduals

--- coronary_ref_homo.py
+++ coronary_ref_homo.py
@@ -14,11 +14,11 @@ class CoronaryRefHomo:
         self.result_cursor: sqlite3.Cursor = result_cursor
         self.data_cursor: sqlite3.Cursor = data_cursor
 
-        sql:str = "SELECT rsID, Risk_allele FROM coronary_disease WHERE state = 'ref' AND zygosity = 'hom'"
+        sql:str = "SELECT rsID, Ref_allele FROM coronary_disease WHERE state = 'ref' AND zygosity = 'hom'"
         self.data_cursor.execute(sql)
         rows:list = self.data_cursor.fetchall()
-        for rsid, risk_allele in rows:
-            self.rsid_map[rsid] = {'exist':True, 'risk':risk_allele}
+        for rsid, ref_allele in rows:
+            self.rsid_map[rsid] = {'exist':True, 'ref':ref_allele}
 
 
     def process_row(self, row):
@@ -37,11 +37,11 @@ class CoronaryRefHomo:
     def end(self) -> None:
         for rsid in self.rsid_map:
             if self.rsid_map[rsid]['exist']:
-                risk:str = self.rsid_map[rsid]['risk']
-                risk = risk+risk
+                ref:str = self.rsid_map[rsid]['ref']
+                ref = ref+ref
 
                 query:str = "SELECT Risk_allele, Gene, Genotype, Conclusion, Weight, PMID, Population, GWAS_study_design, P_value " \
-                        f"FROM coronary_disease WHERE rsID = '{rsid}' AND Genotype = '{risk}';"
+                        f"FROM coronary_disease WHERE rsID = '{rsid}' AND Genotype = '{ref}';"
 
                 self.data_cursor.execute(query)
                 row:list = self.data_cursor.fetchone()
